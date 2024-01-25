d5a46f020018c22f3f268c2d9efb951ca17d9231
IamPrommate <prommatew@gmail.com>
2024-01-25 22:22:21 +0700

    okpa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
-// oiojoijijioj
+import styled, { ThemeProvider } from 'styled-components';
+import { greenTheme } from './utils/Theme';
+import { darkTheme2 } from './utils/Theme';
+import NavBar from './components/NavBar';
+import HeroSec from './components/HeroSec';
+import Skills from './components/Skills';
+import Edu from './components/Edu';
+import Footer from './components/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg };
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+    38.73deg,
+    rgba(21,210,112, 0.15) 0%, 
+    rgba(201, 32, 184, 0) 50%
+  ),
+  linear-gradient(
+    141.27deg,
+    rgba(0, 70, 209, 0) 50%,
+    rgba(0, 70, 209, 0) 100%
+  );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={greenTheme}>
+      <Router>
+      <NavBar />
+      <Body>
+        <HeroSec />
+        <Wrapper>
+          <Skills />
+          <Edu />
+          </Wrapper>
+          <Footer />
+      </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
